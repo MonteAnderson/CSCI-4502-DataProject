4b7c3cefbd2908d2ff2616375ba397fc71e99c77
Monte Anderson <MRAnd023@gmail.com>
2017-12-04 00:35:50 -0700

    Updated the code, more effiecent.

--- GraphData.py
+++ GraphData.py
@@ -14,42 +14,44 @@ if __name__ == "__main__":
     print("TestNorm")
     newListDate = []
     newListPrecip = []
-    count = 0
+    newListTemp = []
     myDict = {}
-    i = 0
-    countedList = []
-    od = {}
-    with open('Crash_Data.csv', 'rU') as fin:
-        #headerLine = next(fin)
+    newDict = {}
+    count = 0
+    with open('ToC-NC-weather.csv', 'rU') as fin:
+        headerLine = next(fin)
         for row in csv.reader(fin):
-            #print(row[1])
-            if ('2016' in row[3]):
-                item = str(row[3])
-                #print(item[0:10])
-                newListDate.append(item[0:10])
+            if ('2016' in row[0]):
+                newListDate.append(str(row[0]))
+	        newListTemp.append(row[1])
+                newListPrecip.append(row[3])
 
-                #newListPrecip.append(row[3])
-                #print(row[2] + "  |  " + str(row[3]))
+        with open('test.csv', 'wb') as fileOut:
+            fileOut.write("Date,Temp,\n")
+            myDict = dict(zip(newListDate, newListTemp))
+            testZip = zip(newListDate, newListTemp, newListPrecip)
+            testZip.sort(key = lambda t: t[0])
 
+            for a,b,c in testZip:
+                print(a,b,c)
+                fileOut.write(a)
+                fileOut.write(",")
+                fileOut.write(b)
+                fileOut.write(",")
+                fileOut.write(c)
+                fileOut.write("\n")
+
+            #print(myDict)
+            #orderedMyDict = collections.OrderedDict(sorted(myDict.items())) 
+
+            """for key, value in orderedMyDict.items():
+                fileOut.write(key)
+                fileOut.write(",")
+                fileOut.write(value)
+
+                fileOut.write("\n")"""
+                
         newListDate.sort()
         #print(Counter(newListDate))
-    with open('test.csv', 'wb') as fileOut:
-
-        countedList = Counter(newListDate)
-        od = collections.OrderedDict(sorted(countedList.items())) 
-        for key, value in od.items():
-            myDict = (key, value)
-    
-            fileOut.write(key) 
-            fileOut.write('\t' + str(value))
-            fileOut.write('\n')
-            #print(myDict)
-
-        """while (i < len(newListDate)):
-            fileOut.write(newListDate[i])
-            fileOut.write('\n')
-            i += 1"""
-
-
         #plt.plot(newListDate,newListPrecip)
         #plt.show()
