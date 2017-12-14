2b9be8c62fdf9db42f20598f9344d80856c76bfe
Monte Anderson <MRAnd023@gmail.com>
2017-12-13 23:01:18 -0700

    Add files via upload

--- ToC_NC_Results/PredictionModel.py
+++ ToC_NC_Results/PredictionModel.py
@@ -14,6 +14,7 @@ from collections import Counter
 if __name__ == "__main__":
     print("Prediction Model Created.")
     dateList = []
+    file2012 = []
     file2013 = []
     file2014 = []
     file2015 = []
@@ -29,51 +30,57 @@ if __name__ == "__main__":
         for row in csv.reader(fin):
             dateList.append(row[0])
 
+    with open('2012-information-ToC-NC.csv', 'rU') as fin:
+        headerLine = next(fin)
+        for row in csv.reader(fin):
+            file2012.append(row[6])
+
     with open('2013-information-ToC-NC.csv', 'rU') as fin:
         headerLine = next(fin)
         for row in csv.reader(fin):
-            file2013.append(row[1])
+            file2013.append(row[6])
 
     with open('2014-information-ToC-NC.csv', 'rU') as fin:
         headerLine = next(fin)
         for row in csv.reader(fin):
-            file2014.append(row[1])
+            file2014.append(row[6])
 
     with open('2015-information-ToC-NC.csv', 'rU') as fin:
         headerLine = next(fin)
         for row in csv.reader(fin):
-            file2015.append(row[1])
+            file2015.append(row[6])
 
     with open('2016-information-ToC-NC.csv', 'rU') as fin:
         headerLine = next(fin)
         for row in csv.reader(fin):
-            file2016.append(row[1])
+            file2016.append(row[6])
 
     with open('2017-information-ToC-NC.csv', 'rU') as fin:
         headerLine = next(fin)
         for row in csv.reader(fin):
-            file2017.append(row[1])
+            file2017.append(row[6])
 
     while(len(file2017) < 365):
         file2017.append(file2016[len(file2017)])
 
-    numZip = zip(file2013, file2014, file2015, file2016, file2017)
+    numZip = zip(file2012, file2013, file2014, file2015, file2016, file2017)
 
-    for year2013,year2014, year2015, year2016,year2017 in numZip:
-        difference = (float(year2013) + float(year2014) + float(year2015) + float(year2016) + float(year2017))/5
+    for year2012,year2013, year2014, year2015, year2016,year2017 in numZip:
+        #print((float(year2012) + float(year2013))/2)
+        difference = ((float(year2012) + float(year2013) + float(year2014) + float(year2015) + float(year2016) + float(year2017))/6)
         predictionModel.append(float(difference))
 
     while (i < 365):
         yearPrediction.append(predictionModel[i])
         i+=1
 
-    #predictedZip = zip(dateList, file2013, file2014, yearPrediction)
-
-    with open('PREDICTION_MODEL_OUTPUT_2018.csv', 'wb') as fileOut:
-        fileOut.write("Date,2013, 2014, 2015, 2016, 2017, PREDICTED FOR 2018\n")
+    with open('PREDICTION_MODEL_OUTPUT_CRASHES_2018.csv', 'wb') as fileOut:
+        fileOut.write("Date,2012,2013,2014,2015,2016,2017,PREDICTED FOR 2014\n")
         i = 0
         while (i < 365):
             fileOut.write(dateList[i])
+            fileOut.write(",")
+            fileOut.write(file2012[i])
             fileOut.write(",")
             fileOut.write(file2013[i])
             fileOut.write(",")
